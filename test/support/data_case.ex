6efb7db41802cdd26ca5860e4de99a7cad866052
fazaimron27 <cincaumyself@gmail.com>
2026-07-20 17:58:01 +0700

    test(support): add Ecto sandbox test helper

---
+++ test/support/data_case.ex
@@ -0,0 +1,26 @@
+defmodule Playmark.DataCase do
+  @moduledoc """
+  Test case for tests that touch the database.
+
+  This is a single-user local app on SQLite, so rather than the SQL sandbox
+  (which fights SQLite's single-writer model), each test starts from an empty
+  `bookmarks` table. Tests using this case must not be `async: true`.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      alias Playmark.Repo
+      import Ecto.Query
+    end
+  end
+
+  setup do
+    Playmark.Repo.delete_all(Playmark.Bookmark)
+    Playmark.Repo.delete_all(Playmark.Subscription)
+    Playmark.Repo.delete_all(Playmark.Playlist)
+    Playmark.Repo.delete_all(Playmark.QueueItem)
+    :ok
+  end
+end
